e1731716644ab449518b0a8d07697b4cf806b721
gaeranmari <mikaelshin98@gmail.com>
2022-06-12 23:35:43 -0300

    adicionando home.js

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,8 +1,11 @@
 const { urlencoded } = require('body-parser');
+const { helloWorld } = require('./controller/home');
 const routeUrl = require('./controller/url_controller');
 
 module.exports = (app) => {
   app.use(urlencoded({ extended: true }));
+
+  app.get('/', helloWorld);
 
   app.get('/url', routeUrl.getUrl);
   app.post('/url', routeUrl.addUrl);
