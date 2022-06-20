4a18bab4aaa2653778889ab34cdf7fe7a4f9b1dd
nkzren <renan.nakazawa0@gmail.com>
2022-06-20 10:12:17 -0300

    [database] adiciona db

---
+++ api/config/init_db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE `esic-catalogo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+CREATE TABLE `esic-catalogo`.`city` (
+  `id` bigint NOT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `domain` varchar(100) DEFAULT NULL,
+  `url` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
