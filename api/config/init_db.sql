8792263728e3bf809d6545ced50ad41d6e56cf06
nkzren <renan.nakazawa0@gmail.com>
2022-07-04 08:45:14 -0300

    corrige script de criacao do banco

--- api/config/init_db.sql
+++ api/config/init_db.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE `esic-catalogo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 CREATE TABLE `esic-catalogo`.`city` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `city` varchar(100) DEFAULT NULL,
   `domain` varchar(100) DEFAULT NULL,
   `url` varchar(512) DEFAULT NULL,
